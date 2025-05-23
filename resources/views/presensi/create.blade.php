f6141ee5c2d110b92eddd13e8a053b8c40fb72ae
Dees <aaryadwisantoso@gmai.com>
2025-05-24 01:10:44 +0700

    lokasi

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -7,20 +7,34 @@
 
     <!-- App Capsule -->
     <div id="appCapsule">
-        <div class="row" style="margin-top: 70px;">
-            <div class="col">
-                <div class= "webcam-capture"></div>
-
-                </div>
+        <style> 
+            .webcam-capture {
               
+            }
+            .webcam-capture video {
+                display: inline-block;
+                width: 100%; !important;
+                height: auto; !important;
+                border-radius: 15px;
+                margin: auto;
             
+               
+            }
+        </style>
+        
+        <div class="row webcam-row-padding" style="margin-top: 70px;">
+            <div class="col d-flex justify-content-center align-items-center flex-column">
+                <input type="text" id="lokasi" class="mb-3" readonly style="max-width:320px;">
+                <div class="webcam-capture"></div>
+            </div>
         </div>
+       
     </div>
-    @push('myscripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js"></script>
     <script> 
         Webcam.set({
-            width: 320,
-            height: 240,
+            width: 400,
+            height: 500,
             image_format: 'jpeg',
             jpeg_quality: 90
         });
@@ -31,7 +45,16 @@
                 document.getElementById('my_camera').innerHTML = '<img src="' + data_uri + '"/>';
             });
         } 
-
+        var lokasi = document.getElementById('lokasi');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(succesCallback, errorCallback); 
+            };
+       function succesCallback(position) {
+           lokasi.value = position.coords.latitude + ',' + position.coords.longitude;
+        }
+        function errorCallback(error) {
+            console.log(error);
+        }
     </script>
     <!-- * App Capsule -->
 
