a203088e8c2385053c4e0993a5782e00c0966780
Rakha <rkhdinar00@gmail.com>
2025-05-24 00:19:04 +0700

    fix

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -10,14 +10,12 @@
         <div class="row" style="margin-top: 70px;">
             <div class="col">
                 <div class= "webcam-capture"></div>
-
-                </div>
-              
-            
+            </div>
         </div>
     </div>
-    @push('myscripts')
-    <script> 
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js"></script>
+    <script language="JavaScript">
         Webcam.set({
             width: 320,
             height: 240,
@@ -27,52 +25,11 @@
         Webcam.attach('.webcam-capture');
 
         function take_snapshot() {
-            Webcam.snap(function (data_uri) {
+            Webcam.snap(function(data_uri) {
                 document.getElementById('my_camera').innerHTML = '<img src="' + data_uri + '"/>';
             });
-        } 
-
+        }
     </script>
     <!-- * App Capsule -->
-
-    <!-- App Bottom Menu -->
-    <div class="appBottomMenu">
-        <a href="#" class="item">
-            <div class="col">
-                <ion-icon name="file-tray-full-outline" role="img" class="md hydrated"
-                    aria-label="file tray full outline"></ion-icon>
-                <strong>Today</strong>
-            </div>
-        </a>
-        <a href="#" class="item">
-            <div class="col">
-                <ion-icon name="calendar-outline" role="img" class="md hydrated"
-                    aria-label="calendar outline"></ion-icon>
-                <strong>Calendar</strong>
-            </div>
-        </a>
-        <a href="#" class="item">
-            <div class="col">
-                <div class="action-button large">
-                    <ion-icon name="camera" role="img" class="md hydrated" aria-label="add outline"></ion-icon>
-                </div>
-            </div>
-        </a>
-        <a href="#" class="item">
-            <div class="col">
-                <ion-icon name="document-text-outline" role="img" class="md hydrated"
-                    aria-label="document text outline"></ion-icon>
-                <strong>Docs</strong>
-            </div>
-        </a>
-        <a href="javascript:;" class="item">
-            <div class="col">
-                <ion-icon name="people-outline" role="img" class="md hydrated"
-                    aria-label="people outline"></ion-icon>
-                <strong>Profile</strong>
-            </div>
-        </a>
-    </div>
-
     <x-layouts.footer />
 </x-layouts.app>
